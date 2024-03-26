3f14abfac54cf66fbcc161cd8f4919409a159a43
Savil Srivastava <676452+savil@users.noreply.github.com>
2024-03-26 09:28:28 -0700

    printing headers in request handlers

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 import (
     "fmt"
+    "os"
     "net/http"
 )
 func hello(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(os.Stderr, "Headers: %+v\n", req.Header)
+
     fmt.Fprintf(w, "default world 6\n")
     w.WriteHeader(http.StatusOK) 
 }
 
 func defaultHandler(w http.ResponseWriter, req *http.Request) {
+  fmt.Fprintf(os.Stderr, "Headers: %+v\n", req.Header)
   fmt.Fprintf(w, "reached the default service handler\n")
   w.WriteHeader(http.StatusOK) 
 }
